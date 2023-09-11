2404d2b5ca20258200e7131303b62fece440edfc
rogerworkup <rogerworkup@gmail.com>
2023-09-10 22:26:04 -0300

    Adicionada logo-mns no GameOver

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react"
 import { QuizContext } from "../context/quiz"
 
-import wellDone from "../img/welldone.svg"
+import mns from "../img/MNS.png"
 
 import "./GameOver.css"
 
@@ -13,7 +13,7 @@ const GameOver = () => {
         <h2>Fim de jogo!</h2>
         <p>Pontuação: {QuizState.score}</p>
         <p>Você acertou {QuizState.score} de {QuizState.questions.length} perguntas.</p>
-        <img src= {wellDone} alt="Fim do Quiz" />
+        <img src= {mns} alt="Fim do Quiz" />
         <button onClick={() => dispatch({type: "NEW_GAME"})}>Reiniciar</button>
     </div>
   )
